131b1f5053464ad3c223beca22dd6c8b5eb8375c
Peter Himschoot <peter@u2u.be>
2020-04-18 15:51:28 +0200

    Add IAggregateRoot marker interface

---
+++ src/U2U.EntityFrameworkCore.Abstractions/BaseClasses/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace U2U.EntityFrameworkCore.Abstractions
+{
+  /// <summary>
+  /// Interface to mark EntityBase as AggregateRoot
+  /// </summary>
+  public interface IAggregateRoot
+  {
+
+  }
+}
